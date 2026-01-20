64d689fd9d362982b448d3a23d36054d50de070b
ReneeLan <reneelan@eztravel.com.tw>
2026-01-20 17:56:19 +0800

    feat: Add deployment scripts and update base path for GitHub Pages hosting

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/2026-spring-busan-trip/',
       server: {
         port: 3000,
         host: '0.0.0.0',
